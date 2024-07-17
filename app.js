99decc15013dfd425bf85d9ed139756195a89879
chisafo <chisafo113@gmail.com>
2024-07-17 21:32:42 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    }))
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
